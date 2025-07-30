fb9216934aec5f5241e93aa785ffa191b1f74ed8
Aswini <2300030072@kluniversity.in>
2025-07-30 10:17:09 +0530

    react project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+function App(){
+  return(
+    <div className="container">
+      <h1>Welcome to My React Project-S105</h1>
+      <p>This is a asimple homepage deployed on github pages using vitr.</p>
+    </div>
+  );
+}
+export default App;
