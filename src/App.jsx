db6bd99dc61afb833c1a16c9c7c1bb04f3256ad4
Aswini <2300030072@kluniversity.in>
2025-07-31 11:52:54 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App(){
     <div className="container">
       <h1>Welcome to My React Project-S105</h1>
       <h1>CI/CD and Cloud DevOps Course</h1>
-      <p>This is a asimple homepage deployed on github pages using vitr.</p>
+      <p>This is a simple homepage deployed on Github Actions using Vite.</p>
     </div>
   );
 }
